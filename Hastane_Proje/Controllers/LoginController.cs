3c355513efec099041f3f3134f632df74876a986
Sude Çakmak <103639801+cakmaksude@users.noreply.github.com>
2023-12-05 23:27:42 +0300

    Login sayfası başlangıç kısmı

---
+++ Hastane_Proje/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Hastane_Proje.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hastane_Proje.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult LogIN(User usr)
+        {
+            return View();
+        }
+    }
+}
---
+++ Hastane_Proje/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hastane_Proje.Models
+{
+    public class User
+    {
+        [Display(Name ="Kullanıcı Adı")]
+        public string kullaniciAdi { get; set; }
+        [Display(Name = "Şifre")]
+        public string kullaniciSifre { get; set; }
+    }
+}
