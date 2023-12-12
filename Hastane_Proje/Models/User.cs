ec798a24509817cf70376b374d9b8834e40945c2
Dilay Kal <104075669+dilaykal@users.noreply.github.com>
2023-12-12 09:00:06 +0300

    Kullanıcı sınıfı

--- Hastane_Proje/Models/User.cs
+++ Hastane_Proje/Models/User.cs
@@ -4,9 +4,32 @@ namespace Hastane_Proje.Models
 {
     public class User
     {
+        [Required(ErrorMessage ="Bu alanı doldurmak zorunludur")]
         [Display(Name ="Kullanıcı Adı")]
         public string kullaniciAdi { get; set; }
+        [Required(ErrorMessage = "Bu alanı doldurmak zorunludur")]
         [Display(Name = "Şifre")]
-        public string kullaniciSifre { get; set; } //
+        public string kullaniciSifre { get; set; }
+        [Required(ErrorMessage = "Bu alanı doldurmak zorunludur")]
+        [Display(Name = "İsim")]
+        public string isim { get; set; }
+        [Required(ErrorMessage = "Bu alanı doldurmak zorunludur")]
+        [Display(Name = "Soyisim")]
+        public string soyisim { get; set; }
+        [Required(ErrorMessage = "Bu alanı doldurmak zorunludur")]
+        [EmailAddress(ErrorMessage ="Geçerli bir mail adres giriniz.")]
+        [Display(Name = "E-posta")]
+        public string eposta { get; set; }
+        [Required(ErrorMessage = "Bu alanı doldurmak zorunludur")]
+        [Display(Name = "Telefon Numarası")]
+        public string telNo { get; set; }
+        [Required(ErrorMessage = "Bu alanı doldurmak zorunludur")]
+        [Display(Name = "TC Kimlik Numarası")]
+        public string tcNo { get; set; }
+        [Display(Name = "Adres")]
+        public string adres { get; set; }
+
+
+
     }
 }
