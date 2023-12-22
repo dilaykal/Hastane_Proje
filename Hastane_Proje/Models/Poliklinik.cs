3a82fea6437462529d3b92f6b7341c37f8f59740
Sude Çakmak <103639801+cakmaksude@users.noreply.github.com>
2023-12-22 14:15:33 +0300

    deneme

--- Hastane_Proje/Models/Poliklinik.cs
+++ Hastane_Proje/Models/Poliklinik.cs
@@ -6,8 +6,8 @@ namespace Hastane_Proje.Models
     {
         public int PoliklinikID { get; set; }
         public string PoliklinikAdi { get; set; }
-        public string Aciklama { get; set; }
-        public string TedaviAlani { get; set; }
+        public string ?Aciklama { get; set; }
+        public string ?TedaviAlani { get; set; }
         public ICollection<Doktor> ?Doktorlar { get; set; }
 
     }
