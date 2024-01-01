b3c84f30b8c791c99100a0681ed2b0695a7c8cbd
Dilay Kal <104075669+dilaykal@users.noreply.github.com>
2024-01-01 16:42:32 +0300

    admin paneli

--- Hastane_Proje/Models/HastaneContext.cs
+++ Hastane_Proje/Models/HastaneContext.cs
@@ -3,7 +3,7 @@ using System.Drawing;
 
 namespace Hastane_Proje.Models
 {
-    public class HastaneContext : IdentityDbContext
+    public class HastaneContext : DbContext
     {
         public DbSet<Doktor> Doktorlar { get; set; }  //Kitap isimli sınıfım Kitaplar isimli bir tabloya karşılık gelsin
         public DbSet<Poliklinik> Poliklinikler { get; set; }
